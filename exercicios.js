89111a0618826f47b04616c0cb8b87bba4c45e69
Martin Perez Tovar <martinpereztovar@gmail.com>
2023-03-22 18:04:22 -0300

    ex 11, 12, 13, 14 e 15

--- exercicios.js
+++ exercicios.js
@@ -3,15 +3,15 @@
 // EXERCÍCIO 0A
 function soma(num1, num2) {
   // implemente sua lógica aqui
-  return num1 + num2
+  return num1 + num2;
 }
 
 // EXERCÍCIO 0B
 function imprimeMensagem() {
   // implemente sua lógica aqui
-  const mensagem = prompt('Digite uma mensagem!')
+  const mensagem = prompt("Digite uma mensagem!");
 
-  console.log(mensagem)
+  console.log(mensagem);
 }
 
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
@@ -28,13 +28,13 @@ function calculaAreaRetangulo(altura, largura) {
 function imprimeIdade(anoAtual, anoDeNascimento) {
   anoAtual = Number(prompt("Digite o ano atual:"));
   anoDeNascimento = Number(prompt("Digite o ano de nascimento:"));
-  const idade = anoAtual - anoDeNascimento
-  console.log(idade)
+  const idade = anoAtual - anoDeNascimento;
+  console.log(idade);
 }
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
-  const imc = (peso / (altura * altura));
+  const imc = peso / (altura * altura);
   return imc.toFixed(1);
 }
 
@@ -42,8 +42,10 @@ function calculaIMC(peso, altura) {
 function imprimeInformacoesUsuario(nome, idade, email) {
   nome = prompt("Digite seu nome:");
   idade = Number(prompt("Digite sua idade:"));
-  email = prompt("Digite seu email:")
-  console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`); 
+  email = prompt("Digite seu email:");
+  console.log(
+    `Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`
+  );
 }
 
 // EXERCÍCIO 05
@@ -51,8 +53,8 @@ function imprimeTresCoresFavoritas(cor1, cor2, cor3) {
   cor1 = prompt("Digite uma cor favorita:");
   cor2 = prompt("Digite outra cor favorita:");
   cor3 = prompt("Digite outra cor favorita:");
-  
-  let cores = [cor1, cor2, cor3]
+
+  let cores = [cor1, cor2, cor3];
   console.log(cores);
 }
 
@@ -82,37 +84,59 @@ function retornaPrimeiroElemento(array) {
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
-  const ultimoElemento = array[array.length - 1]; 
+  const ultimoElemento = array[array.length - 1];
   return ultimoElemento;
-
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
-  // implemente sua lógica aqui
+  const getFirstElement = array[0];
+  array[0] = array[array.length - 1];
+  array[array.length - 1] = getFirstElement;
 
+  return array;
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
-  // implemente sua lógica aqui
-
+  return string1.toLowerCase() === string2.toLowerCase();
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
-  // implemente sua lógica aqui
+  currentYear = Number(prompt("ano atual: "));
+  birthYear = Number(prompt("ano de nascimento: "));
+  idYear = Number(prompt("ano de emissao do RG: "));
+  const age = currentYear - birthYear;
+  let renoval = currentYear - idYear;
+  let needRenoval = false;
 
+  if (age <= 20 && renoval >= 5){
+    needRenoval = true;
+  }
+  else if (age > 20 && age <= 50 && renoval >= 10){
+    needRenoval = true;
+  }
+  else if(age > 50 && renoval >= 15){
+    needRenoval = true;
+  }
+  console.log(needRenoval); 
 }
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
-  // implemente sua lógica aqui
+  const multiplo400 = ano % 400 === 0; // true
+  const multiplo100 = ano % 100 === 0; // false
+  const multiplo4 = ano % 4 === 0; // true
 
+  return multiplo400 || (multiplo4 && !multiplo100);
 }
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
-  // implemente sua lógica aqui
+  const legalAge = prompt("Você tem mais de 18 anos? Responda as perguntas com 'Sim' ou 'não'");
+  const completedHichschool = prompt("Você possui ensino médio completo?");
+  const availability = prompt("Você possui disponibilidade exclusiva durante os horários do curso?");
 
+  console.log(legalAge === ("sim" || "Sim") && completedHichschool === ("sim" || "Sim") && availability === ("sim" || "Sim"));
 }
